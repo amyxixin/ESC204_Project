9b829fc8ba280b5ede9cc4748256a5fcb4a2b0c6
amyxixin <amyxin18@hotmail.com>
2020-02-29 19:01:46 -0500

    add bluetooth control

---
+++ Milestone 1/Bluetooth_Control/Bluetooth_Control.ino
@@ -0,0 +1,153 @@
+//motor connections
+#define EnA 2
+#define EnB 3
+#define EnC 4
+#define EnD 5
+
+//top and bottom
+#define InB1 30
+#define InB2 31
+#define InB3 32
+#define InB4 33
+
+//left and right
+#define InA1 22
+#define InA2 23
+#define InA3 24
+#define InA4 25
+
+//encoder connections
+#define EncL1 18  //left motor
+#define EncL2 40
+#define EncT1 19  //top motor
+#define EncT2 41
+#define EncR1 20  //right motor
+#define EncR2 42
+#define EncB1 21  //bottom motor
+#define EncB2 43
+
+char val = 0;
+int normalSpeed = 255;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("starting...");
+
+  //initializing motors
+  pinMode(EnA, OUTPUT);
+  pinMode(EnB, OUTPUT);
+  pinMode(EnC, OUTPUT);
+  pinMode(EnD, OUTPUT);
+  
+  pinMode(InA1, OUTPUT);
+  pinMode(InA2, OUTPUT);
+  pinMode(InA3, OUTPUT);
+  pinMode(InA4, OUTPUT);
+  
+  pinMode(InB1, OUTPUT);
+  pinMode(InB2, OUTPUT);
+  pinMode(InB3, OUTPUT);
+  pinMode(InB4, OUTPUT);
+}
+
+void loop() {
+  if (Serial.available()){
+    val = Serial.read();
+    Serial.println(val);
+    if (val == 'w'){
+      setPosY();
+      moveY();
+    } else if (val == 'a'){
+      setNegX();
+      moveX();
+    } else if (val == 's'){
+      setNegY();
+      moveY();
+    } else if (val == 'd'){
+      setPosX();
+      moveX();
+    } else if (val == 'k'){
+      stopMotors();
+    }
+  }
+}
+
+void setSpeedY(int power){
+  if(power > 100){
+    analogWrite(EnA, power);
+    analogWrite(EnB, power);
+  }else{
+    digitalWrite(EnA, LOW);
+    digitalWrite(EnB, LOW);
+  }
+}
+
+void setSpeedX(int power){
+  if(power > 100){
+    analogWrite(EnC, power);
+    analogWrite(EnD, power);
+  }else{
+    digitalWrite(EnC, LOW);
+    digitalWrite(EnD, LOW);
+  }
+}
+
+void setPosY(){
+  //towards charging port
+  digitalWrite(InA1, LOW);
+  digitalWrite(InA2, HIGH);
+  digitalWrite(InA3, LOW);
+  digitalWrite(InA4, HIGH);
+}
+
+void setNegY(){
+  //away from charging port
+  digitalWrite(InA1, HIGH);
+  digitalWrite(InA2, LOW);
+  digitalWrite(InA3, HIGH);
+  digitalWrite(InA4, LOW);
+}
+
+void setPosX(){
+  //right
+  digitalWrite(InB1, LOW);
+  digitalWrite(InB2, HIGH);
+  digitalWrite(InB3, HIGH);
+  digitalWrite(InB4, LOW);
+}
+
+void setNegX(){
+  //left
+  digitalWrite(InB1, HIGH);
+  digitalWrite(InB2, LOW);
+  digitalWrite(InB3, LOW);
+  digitalWrite(InB4, HIGH);
+}
+
+void stopMotors(){
+  digitalWrite(InA1, LOW);
+  digitalWrite(InA2, LOW);
+  digitalWrite(InA3, LOW);
+  digitalWrite(InA4, LOW);
+  
+  digitalWrite(InB1, LOW);
+  digitalWrite(InB2, LOW);
+  digitalWrite(InB3, LOW);
+  digitalWrite(InB4, LOW);
+}
+
+void moveY(){
+  //turn on both motors
+  analogWrite(EnA, normalSpeed);
+  analogWrite(EnB, normalSpeed);
+  analogWrite(EnC, 0);
+  analogWrite(EnD, 0);
+}
+
+void moveX(){
+  //turn on both motors
+  analogWrite(EnA, 0);
+  analogWrite(EnB, 0);
+  analogWrite(EnC, normalSpeed);
+  analogWrite(EnD, normalSpeed);
+}
--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,23 +1,47 @@
 int stepPin = 3;
 int dirPin = 2;
-int pause = 1000;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(dirPin, LOW);
+  digitalWrite(dirPin, HIGH);
 
-  for(int x = 0; x < 200; x++){
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(pause);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(pause);
-  }
+//  if (Serial.available() > 0) {  
+//    int integerValue = 0;        
+//    while(1) {           
+//      char incomingByte = Serial.read();
+//      if (incomingByte == '\n') break;   
+//      if (incomingByte == -1) continue;  
+//      integerValue *= 10;  
+//      integerValue = ((incomingByte - 48) + integerValue);
+//      Serial.println(String(integerValue));
+//      if(integerValue > 0 && integerValue < 500){
+//        raiseLeadScrew(integerValue);
+//      }
+//    }
+//  }
+//  
+//  delay(10);
 
+  raiseLeadScrew(100);
   delay(1000);
+}
+
+void raiseLeadScrew(int d) {
+  const int p = 1000;
+
+  int numTurns = floor(d * (200/8));
+  
+  for(int x = 0; x < numTurns; x++){
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(p);
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(p);
+  }
 }
